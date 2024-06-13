49b9cf63eb372d1f784991bcb1ebd8abeab3738d
Nagamatsu Ryosuke <koukapoke@gmail.com>
2024-06-13 14:04:44 +0900

    add ImageModal

--- src/components/ui/DeckCard/DeckCard.tsx
+++ src/components/ui/DeckCard/DeckCard.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import { Code2Image } from "../../../utils/Code2Image";
+import ImageModal from "../ImageModal/ImageModal";
 
 export const DeckCard = (props: { deckCode: string, deckType: string }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = (imageUrl: string) => {
+    // setSelectedImage(imageUrl);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    // setSelectedImage(null);
+  };
+
   return (
     <div className="w-full bg-[#C8F2FF] p-3 rounded-lg shadow-xl">
       <img
         className="w-full h-auto"
         src={Code2Image(props.deckCode)}
         alt={props.deckCode}
+        onClick={() => openModal(Code2Image(props.deckCode))}
       />
       {props.deckType === "不正なデッキコード" && (
         <div className="text-center font-m-plus-1p mt-2 text-red-600">
@@ -23,6 +38,9 @@ export const DeckCard = (props: { deckCode: string, deckType: string }) => {
           {props.deckCode}
         </a>
       </div>
+      {modalIsOpen && (
+        <ImageModal isOpen={modalIsOpen} onClose={closeModal} imageUrl={Code2Image(props.deckCode)} />
+      )}
     </div>
   );
 }
